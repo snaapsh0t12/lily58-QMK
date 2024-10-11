f063e37c6d02261d0c00e3286d165c9a411264fd
Aiden Lee <aidenjlee9478@gmail.com>
2024-10-10 23:27:15 -0500

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -22,7 +22,10 @@ enum layer_number {
 };
 
 enum custom_keycodes {
-    CLOSE_WINDOW = SAFE_RANGE
+    CLOSE_WINDOW = SAFE_RANGE,
+    COPY,
+    PASTE,
+    CUT
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -30,6 +33,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case CLOSE_WINDOW:
             if (record->event.pressed) {
                 SEND_STRING(SS_DOWN(X_LALT)SS_TAP(X_F4)SS_UP(X_LALT));
+            }
+        case COPY:
+            if (record->event.pressed) {
+                SEND_STRING(SS_DOWN(X_LCTL)SS_TAP(X_C)SS_UP(X_LCTL));
+            }
+        case PASTE:
+            if (record->event.pressed) {
+                SEND_STRING(SS_DOWN(X_LCTL)SS_TAP(X_V)SS_UP(X_LCTL));
+            }
+        case CUT:
+            if (record->event.pressed) {
+                SEND_STRING(SS_DOWN(X_LCTL)SS_TAP(X_X)SS_UP(X_LCTL));
             }
             break;
     }
@@ -102,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   CLOSE_WINDOW, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,\
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
   KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD, \
-  _______, DF(_NEW_QWERTY), _______, _______, _______, _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+  _______, DF(_NEW_QWERTY),     CUT,   COPY, PASTE, _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
                              _______, _______, _______, _______, _______,  _______, _______, _______\
 ),
 /* RAISE
